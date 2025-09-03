ff9e9873c84019f6059ec9ed7d16da0a763412a9
clementw168 <clement.wang@mirakl.com>
2025-09-03 10:27:47 +0200

    refactor words

--- player.py
+++ player.py
@@ -10,18 +10,35 @@
 
 # List of available words, taken from the Small World of Words
 
+import pdb
+
 import numpy as np
 from sknetwork.data import load_netset
+
 from create_embeddings import create_or_load_embeddings
-import pdb
 
 data = load_netset("swow")
 adjacency = data.adjacency  # graph (if needed)
 words = [str(word) for word in data.names]  # words
+word_to_index = {word: i for i, word in enumerate(words)}
 
 # Functions to complete; your code must run fast (less than 100ms on a laptop)
 
 table = create_or_load_embeddings(words, model_name="all-MiniLM-L6-v2", batch_size=128)
+
+
+def get_turn(roles):
+    return len(roles)
+
+
+def convert_word_list_to_embeddings(word_list):
+    word_indices = np.array([word_to_index[word] for word in word_list])
+    return table[word_indices]
+
+
+def cosine_distance(embeddings1, embeddings2):
+    return -np.dot(embeddings1, embeddings2.T)
+
 
 def speak(
     n_players, player, secret_word="", list_words=[], list_players=[], roles=dict()
@@ -56,23 +73,30 @@ def speak(
     > "sleep"
     """
     if len(secret_word):
-        return speak_adjacency(n_players, player, secret_word, list_words, list_players, roles)
+        return speak_adjacency(
+            n_players, player, secret_word, list_words, list_players, roles
+        )
     else:
-        return speak_random(words, n_players*player % len(words))
+        return speak_random(words, n_players * player % len(words))
+
 
 def speak_random(words, seed):
     return words[seed]
 
-def speak_adjacency(n_players, player, secret_word="", list_words=[], list_players=[], roles=dict()) -> str:
+
+def speak_adjacency(
+    n_players, player, secret_word="", list_words=[], list_players=[], roles=dict()
+) -> str:
     pdb.set_trace()
     # find words adjacent to secret_word, enough to have a new word
 
 
 def compute_distance(main_word, other_words):
-    d = np.expand_dims(table[main_word], axis=0) - np.concatenate([table[w] for w in other_words], axis=0)
+    d = np.expand_dims(table[main_word], axis=0) - np.concatenate(
+        [table[w] for w in other_words], axis=0
+    )
     d = np.linalg.norm(d, axis=1)
     return d
-
 
 
 def vote(
@@ -104,20 +128,40 @@ def vote(
     > 2
     """
     if len(secret_word):
-        return vote_nonwhite(n_players, player, secret_word, list_words, list_players, roles)
+        return vote_nonwhite(
+            n_players, player, secret_word, list_words, list_players, roles
+        )
     else:
         return vote_white(n_players, player, list_words, list_players, roles)
 
-def vote_nonwhite(n_players, player, secret_word="", list_words=[], list_players=[], roles=dict()) -> int:
-    distances = compute_distance(secret_word, list_words)
-    closest = np.argmin(distances, axis=0).int()
-    return list_players[closest]
+
+def vote_nonwhite(
+    n_players, player, secret_word="", list_words=[], list_players=[], roles=dict()
+) -> int:
+    turn = get_turn(roles)
+    current_words = list_words[: n_players - turn]
+    current_players = list_players[: n_players - turn]
+
+    if secret_word in current_words:
+        player_who_said_secret_word = current_players[current_words.index(secret_word)]
+        return player_who_said_secret_word
+
+    current_words_embeddings = convert_word_list_to_embeddings(current_words)
+    secret_word_embedding = convert_word_list_to_embeddings([secret_word])
+    distances = cosine_distance(secret_word_embedding, current_words_embeddings)
+
+    farthest_player = np.argmax(distances, axis=0).int()
+    return current_players[farthest_player]
+
 
 def vote_white(n_players, player, list_words=[], list_players=[], roles=dict()) -> int:
-    votei = 0
-    while list_players[votei]==player:
-        votei+=1
-    return list_players[votei]
+    # Clustering, into voter random dans le cluster des undercover
+    remaining_players = [
+        i for i in range(1, n_players + 1) if i != player and i not in roles
+    ]
+    random_player = np.random.choice(remaining_players)
+    return random_player
+
 
 def guess(n_players, player, list_words=[], list_players=[], roles=dict()) -> str:
     """
