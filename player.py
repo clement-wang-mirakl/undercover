5c93108ec3400dbeef17b2d8b1b8e687064fa2dd
clementw168 <clement.wang@mirakl.com>
2025-09-03 11:36:56 +0200

    put all in player.py

--- player.py
+++ player.py
@@ -10,19 +10,51 @@
 
 # List of available words, taken from the Small World of Words
 
+import os
 import pdb
 
 import numpy as np
 from sknetwork.data import load_netset
+from tqdm import tqdm
 
-from create_embeddings import create_or_load_embeddings
-
+# Load the data
 data = load_netset("swow")
-adjacency = data.adjacency  # graph (if needed)
-words = [str(word) for word in data.names]  # words
+adjacency = data.adjacency
+words = [str(word) for word in data.names]
 word_to_index = {word: i for i, word in enumerate(words)}
 
+
 # Functions to complete; your code must run fast (less than 100ms on a laptop)
+
+
+EMBEDDING_FILE = "embeddings.npy"
+
+
+def create_or_load_embeddings(words, model_name="all-MiniLM-L6-v2", batch_size=128):
+    if os.path.exists(EMBEDDING_FILE):
+        print(f"Embeddings already exist, loading from {EMBEDDING_FILE}")
+        embeddings = np.load(EMBEDDING_FILE)
+        return embeddings
+
+    from sentence_transformers import SentenceTransformer
+
+    model = SentenceTransformer(model_name)
+
+    # Create embeddings for all words
+    embeddings = []
+    for word in tqdm(range(0, len(words), batch_size), desc="Creating embeddings"):
+        batch = words[word : word + batch_size]
+        batch_embeddings = model.encode(batch)
+
+        embeddings.append(batch_embeddings)
+
+    embeddings = np.concatenate(embeddings, axis=0)
+
+    np.save(EMBEDDING_FILE, embeddings)
+    print(f"Embeddings shape: {embeddings.shape}")
+    print(f"Embeddings saved to {EMBEDDING_FILE}")
+    return embeddings
+
 
 table = create_or_load_embeddings(words, model_name="all-MiniLM-L6-v2", batch_size=128)
 
